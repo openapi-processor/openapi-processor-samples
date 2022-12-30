d1b7671568c7767782210eb9f6445517c13c950c
Martin Hauner <martin.hauner@gmx.net>
2022-12-30 16:26:51 +0100

    new sample 2032.1

---
+++ samples/spring-mvc-boot-3/build.gradle.kts
@@ -0,0 +1,88 @@
+// this build file is using a version catalog (see settings.gradle.kts)
+
+@Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
+plugins {
+    id("java")
+    id("groovy")
+    alias(libs.plugins.boot3)
+    alias(libs.plugins.boot.deps)
+    alias(libs.plugins.versions)
+
+    // add processor-gradle plugin
+    alias(libs.plugins.processor.gradle)
+}
+
+group = "io.openapiprocessor"
+version = "0.0.1-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+// configure an openapi-processor inside the 'openapiProcessor' configuration by adding a nested
+// configuration with the name of the openapi-processor and its options inside it.
+//
+// ... using 'spring' and 'json'.
+openapiProcessor {
+
+    // the path to the open api yaml file. Usually the same for all processors.
+    apiPath("$projectDir/src/api/openapi.yaml")
+
+    // based on the name of the processor configuration the plugin creates a gradle task with name
+    // "process${name of processor}"  (in this case "processSpring") to run the processor.
+    process("spring") {
+        // the spring processor dependency
+        // processor("io.openapiprocessor:openapi-processor-spring:2023.1-SNAPSHOT")
+        processor("${libs.processor.spring.get()}")
+
+        // setting api path inside a processor configuration overrides the one at the top.
+        // apiPath "${projectDir}/src/api/openapi.yaml"
+
+        // the destination folder for generating interfaces & models. This is the parent of the
+        // {package-name} folder tree configured in the mapping file.
+        targetDir("$projectDir/build/openapi")
+
+        // processor specific options, creates a key => value map that is passed to the processor
+
+        // file name of the mapping yaml configuration file. Note that the yaml file name must end
+        // with either {@code .yaml} or {@code .yml}.
+        prop("mapping", "$projectDir/src/api/mapping.yaml")
+
+        // sets the parser to SWAGGER or INTERNAL. if not set SWAGGER is used.
+        // INTERNAL provides full JSON schema validation
+        prop("parser", "INTERNAL")
+
+        // alternative way of setting processor specific properties
+        /*
+        prop(mapOf(
+            "mapping" to "$projectDir/src/api/mapping.yaml",
+            "parser" to "INTERNAL"
+        ))
+         */
+    }
+}
+
+// add the targetDir of the processor as additional source folder to java.
+sourceSets {
+    main {
+        java {
+            // add generated files
+            srcDir("$projectDir/build/openapi")
+        }
+    }
+}
+
+// generate api before compiling
+tasks.compileJava {
+    dependsOn("processSpring")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
---
+++ samples/spring-mvc-boot-3/src/main/java/io/openapiprocessor/samples/FooBar.java
@@ -0,0 +1,4 @@
+package io.openapiprocessor.samples;
+
+public @interface FooBar {
+}
