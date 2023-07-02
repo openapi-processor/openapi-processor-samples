a27b0767861df8602e1fe3f54c3fe48226d14378
Martin Hauner <martin.hauner@gmx.net>
2023-07-02 23:18:29 +0200

    dummy implementation

--- samples/spring-mvc-boot-3-kt/src/main/kotlin/io/openapiprocessor/samples/ApiController.kt
+++ samples/spring-mvc-boot-3-kt/src/main/kotlin/io/openapiprocessor/samples/ApiController.kt
@@ -4,15 +4,16 @@ import io.openapiprocessor.openapi.api.BarApi
 import io.openapiprocessor.openapi.api.FooApi
 import io.openapiprocessor.openapi.model.Foo
 import org.springframework.web.bind.annotation.RestController
+import java.util.*
 
 @RestController
 class ApiController: BarApi, FooApi {
 
     override fun postBar(bar: MappedBar?): MappedBar {
-        TODO("Not yet implemented")
+        return bar ?: MappedBar()
     }
 
     override fun postFoo(foo: Foo?): Foo {
-        TODO("Not yet implemented")
+        return foo ?: Foo("foo", UUID.randomUUID(), MappedBar())
     }
 }
