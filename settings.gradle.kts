3cc85e23dd6e533038cf381988b156ca711ffed6
Martin Hauner <martin.hauner@gmx.net>
2026-03-04 18:30:05 +0100

    update

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,13 @@
 rootProject.name = "openapi-processor-samples"
 
-//includeBuild("samples/spring-mvc-boot-4-packages-kt")
+includeBuild("samples/spring-mvc-boot-4-packages-kt")
 //includeBuild("samples/spring-mvc-boot-4-stream-kt")
-includeBuild("samples/spring-flux-boot-3-java")
+//includeBuild("samples/spring-flux-boot-3-java")
+//includeBuild("samples/spring-mvc-boot-3-java")
+//includeBuild("samples/spring-mvc-boot-3-kt")
+//includeBuild("samples/spring-mvc-boot-3-validation-java")
+//includeBuild("samples/spring-mvc-boot-3-multiple-apis")
+//includeBuild("samples/spring-mvc-boot-3-monolith-kt")
+//includeBuild("samples/spring-mvc-boot-3-lombok")
+//includeBuild("samples/micronaut-3-java")
+//includeBuild("samples/micronaut-4-kt")
